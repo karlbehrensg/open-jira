380d69e921fcce8dc61c04da0e57533f22d40e56
Karl Behrens <karlbehrensg@gmail.com>
2022-04-20 15:46:31 -0400

    Layout and Navbar

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,23 @@
+import { FC, PropsWithChildren } from "react";
+import Head from "next/head";
+import { Box } from "@mui/material";
+import { Navbar } from "../ui";
+
+interface Props {
+  title?: string;
+}
+
+export const Layout: FC<PropsWithChildren<Props>> = ({ title, children }) => {
+  return (
+    <Box sx={{ flexFlow: 1 }}>
+      <Head>
+        <title>{title}</title>
+      </Head>
+
+      <Navbar />
+      {/* Sidebar */}
+
+      <Box sx={{ padding: "10px 20px" }}>{children}</Box>
+    </Box>
+  );
+};
