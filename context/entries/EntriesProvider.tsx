2b97fb3018354476de40f6e71b211ce22fa00b45
Karl Behrens <karlbehrensg@gmail.com>
2022-04-26 22:36:56 -0400

    Entries interface

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,14 +1,36 @@
 import { FC, PropsWithChildren, useReducer } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+import { Entry } from "../../interfaces";
 import { EntriesContext, entriesReducer } from "./";
 
 interface Props {}
 
 export interface EntriesState {
-  entries: [];
+  entries: Entry[];
 }
 
 const Entries_INITIAL_STATE: EntriesState = {
-  entries: [],
+  entries: [
+    {
+      _id: uuidv4(),
+      description: "This is a demo task",
+      status: "pending",
+      createdAt: Date.now(),
+    },
+    {
+      _id: uuidv4(),
+      description: "This is a demo task",
+      status: "in-progress",
+      createdAt: Date.now() - 1000000,
+    },
+    {
+      _id: uuidv4(),
+      description: "This is a demo task",
+      status: "done",
+      createdAt: Date.now() - 100000,
+    },
+  ],
 };
 
 export const EntriesProvider: FC<PropsWithChildren<Props>> = ({ children }) => {
