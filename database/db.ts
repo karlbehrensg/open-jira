dab3009c01e7d18e4009ad653ae7b71b86e29db4
Karl Behrens <karlbehrensg@gmail.com>
2022-04-28 15:10:40 -0400

    Create mongo connections file

---
+++ database/db.ts
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+/**
+ * 0 = disconnected
+ * 1 = connected
+ * 2 = connecting
+ * 3 = disconnecting
+ */
+
+const mongooConnection = {
+  isConnected: 0,
+};
+
+export const connect = async () => {
+  if (mongooConnection.isConnected) {
+    console.log("MongoDB is already connected");
+    return;
+  }
+
+  if (mongoose.connections.length > 0) {
+    mongooConnection.isConnected = mongoose.connections[0].readyState;
+
+    if (mongooConnection.isConnected === 1) {
+      console.log("Using previous connection");
+      return;
+    }
+
+    await mongoose.disconnect();
+  }
+
+  await mongoose.connect("...");
+  mongooConnection.isConnected = 1;
+  console.log("Connectiong to MongoDB...", "...");
+};
+
+export const disconnect = async () => {
+  if (mongooConnection.isConnected !== 0) return;
+  await mongoose.disconnect();
+  console.log("Disconnected from MongoDB");
+};
